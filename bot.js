0ec4d17c19dc940428cc73c6d255c151c50da8c8
varlock3 <44277067+varlock3@users.noreply.github.com>
2018-11-12 23:03:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
 client.on("guildMemberAdd", (m) => {
-m.guild.channels.get("497089759831916546").send("** Welcome To Throné**  .")
+m.guild.channels.get("497089759831916546").send("**Welcome To Throné**")
 })
 client.login(process.env.BOT_TOKEN);
